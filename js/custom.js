9684af575f4b4c026529289ac6535c021a85e0c2
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-02-15 18:06:54 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(function () {
+    var main_movie_slide = new Swiper(".main_movie_slide", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    var main_visual_wrap = new Swiper(".main_visual_wrap", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+});
