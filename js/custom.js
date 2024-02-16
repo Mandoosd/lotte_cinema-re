7d097585cdc3a4ba4dea3c0ecd80b605d38c45b5
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-02-16 18:13:04 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,32 @@
 $(function () {
-    var main_movie_slide = new Swiper(".main_movie_slide", {
+    var swiperTopNum = $('.main-slide').find('.swiper-slide');
+    var swiperSubNum = $('.sub-slide').find('.sub-slide');
+
+    var mainSlide = new Swiper('.main-slide ', {
+        effect: "fade",
+        spaceBetween: 30,	//슬라이드 간격
+        loop: true,	//슬라이드 반복
+        loopedSlides: swiperTopNum.length
+
+    });
+
+    var subSlide = new Swiper(' .sub-slide ', {
+
+        spaceBetween: 30,	//슬라이드 간격 
+        centeredSlides: false,	//슬라이드 중앙 배치
+        slidesPerView: '3',	//한번에 보여지는 슬라이드 개수
+        loop: true,	//슬라이드 반복
+        loopedSlides: swiperSubNum.length, //loop 시 파라미터 duplicate 개수
+
+
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
     });
-    var main_visual_wrap = new Swiper(".main_visual_wrap", {
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
+
+
+    mainSlide.controller.control = subSlide;
+    subSlide.controller.control = mainSlide;
 
 });
