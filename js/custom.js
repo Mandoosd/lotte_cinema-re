7d8e6cc27d56e91fc481fa69e153685fce0eae7b
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-02-19 18:13:32 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,29 @@
 $(function () {
-    var swiperTopNum = $('.main-slide').find('.swiper-slide');
-    var swiperSubNum = $('.sub-slide').find('.sub-slide');
 
-    var mainSlide = new Swiper('.main-slide ', {
+    var mainSlide = new Swiper('.main_slide_wrap ', {
         effect: "fade",
-        spaceBetween: 30,	//슬라이드 간격
-        loop: true,	//슬라이드 반복
-        loopedSlides: swiperTopNum.length
 
-    });
-
-    var subSlide = new Swiper(' .sub-slide ', {
-
-        spaceBetween: 30,	//슬라이드 간격 
-        centeredSlides: false,	//슬라이드 중앙 배치
-        slidesPerView: '3',	//한번에 보여지는 슬라이드 개수
-        loop: true,	//슬라이드 반복
-        loopedSlides: swiperSubNum.length, //loop 시 파라미터 duplicate 개수
 
 
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+        centeredSlides: true,	//슬라이드 중앙 배치
+        loop: true,	//슬라이드 반복
     });
 
+    // var subSlide = new Swiper(' .sub-slide ', {
 
-    mainSlide.controller.control = subSlide;
-    subSlide.controller.control = mainSlide;
+    //     spaceBetween: 30,	//슬라이드 간격 
+    //     navigation: {
+    //         nextEl: ".swiper-button-next",
+    //         prevEl: ".swiper-button-prev",
+    //     },
+    //     centeredSlides: false,	//슬라이드 중앙 배치
+    //     loop: true,	//슬라이드 반복
+    //     loopedSlides: swiperSubNum.length, //loop 시 파라미터 duplicate 개수
 
+
+    // });
 });
