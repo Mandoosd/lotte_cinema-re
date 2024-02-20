1b0e6719db0e83b059aca2450b05ff6917edaa3d
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-02-20 18:11:30 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,18 @@
 $(function () {
 
     var mainSlide = new Swiper('.main_slide_wrap ', {
-        effect: "fade",
-
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        spaceBetween: 100,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
 
 
         navigation: {
@@ -13,17 +23,4 @@ $(function () {
         loop: true,	//슬라이드 반복
     });
 
-    // var subSlide = new Swiper(' .sub-slide ', {
-
-    //     spaceBetween: 30,	//슬라이드 간격 
-    //     navigation: {
-    //         nextEl: ".swiper-button-next",
-    //         prevEl: ".swiper-button-prev",
-    //     },
-    //     centeredSlides: false,	//슬라이드 중앙 배치
-    //     loop: true,	//슬라이드 반복
-    //     loopedSlides: swiperSubNum.length, //loop 시 파라미터 duplicate 개수
-
-
-    // });
 });
